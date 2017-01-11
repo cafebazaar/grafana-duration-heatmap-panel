6a622b834a38214daa83b1a205bd8fc284c0daa4
Mahdi Mohaveri <mmohaveri@gmail.com>
2017-01-11 16:15:37 +0330

    Minor bug fix in heatmap.js (heatmap should be drawn insed <g>)
    So it be placed in the boundries defined margins.

--- src/heatmap.js
+++ src/heatmap.js
@@ -7,17 +7,22 @@ export default function heatmap(data, num_of_slices, elem, svg_id, elem_size, da
     var width = elem_size.width - margin.left - margin.right;
     var height = elem_size.height - margin.top - margin.bottom;
 
-    var svg = d3.select("svg#"+svg_id).attr('width', width + margin.left + margin.right).attr('height', height + margin.top + margin.bottom);
-
-    //Move the origin of drawing
-    svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var yStep = 1;
 
     var vertical_domain_length = bin_domain.max-bin_domain.min;
     var rect_size = Math.min(width/num_of_slices, height/vertical_domain_length);
 
+    width = rect_size*num_of_slices;
+    height = rect_size*vertical_domain_length;
+
+    var svg = d3.select("svg#"+svg_id).attr('width', width + margin.left + margin.right).attr('height', height + margin.top + margin.bottom)
+                .append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")"); //Move the origin of drawing
+
+
+
     // In getting log we added 1 to each frequency so we never encounter Log(0) situation
-    var x_scale = d3.time.scale().range([0, rect_size*num_of_slices]).domain([date_domain.min, date_domain.max]);
-    var y_scale = d3.scale.linear().range([rect_size*vertical_domain_length, 0]).domain([bin_domain.min, bin_domain.max]);
+    var x_scale = d3.time.scale().range([0, width]).domain([date_domain.min, date_domain.max]);
+    var y_scale = d3.scale.linear().range([height, 0]).domain([bin_domain.min, bin_domain.max]);
     var z_scale = d3.scale.linear().range(['white', 'darkred']).domain([Math.log(frq_domain.min+1), Math.log(frq_domain.max+1)]);
 
 
@@ -28,12 +33,13 @@ export default function heatmap(data, num_of_slices, elem, svg_id, elem_size, da
         .style("opacity", 0);
 
     // Draw heat map tiles
+    // x and y indicates upper left of the rect, hence the "+ yStep" in 'y' is needed to prevent first row from going out of the div.
     svg.selectAll(".tile")
         .data(data)
         .enter()
         .append("rect")
         .attr('x', d => x_scale(d.date))
-        .attr('y', d => y_scale(d.bin))
+        .attr('y', d => y_scale(d.bin + yStep))
         .attr("height", rect_size)
         .attr("width", rect_size)
         .style("fill", d => z_scale(Math.log(d.value+1)))
